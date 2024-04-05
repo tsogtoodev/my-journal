45f8f3bd63b8f58563b8158f2b1a7edba32ab096
Acepmon <dtsogtbayar123@gmail.com>
2024-04-06 04:18:38 +0800

    meaningful activity entry added

--- pages/thoughts.vue
+++ pages/thoughts.vue
@@ -1,5 +1,18 @@
 <template>
   <UContainer class="py-2">
-    you have thoughts?
+    <div class="flex flex-row">
+      <div class="flex flex-col w-64">
+        <span class="text-gray-400 text-sm">2024, April</span>
+        <UVerticalNavigation :links="links" />
+      </div>
+
+      <NuxtPage />
+    </div>
   </UContainer>
 </template>
+
+<script setup lang="ts">
+const links = [
+  {label: "- Meaningful Activity", to: "/thoughts/meaningful-activity"},
+];
+</script>
---
+++ pages/thoughts/meaningful-activity.vue
@@ -0,0 +1,85 @@
+<template>
+  <article class="prose prose-sm mx-auto">
+    <h2>Meaningful activity! is there a point to it?</h2>
+    <small>2024, April 6th - 03:46</small>
+
+    <p>
+      I don't have hobbies or side activities that i do thats considered meaningful
+      to other people. I just watch Youtube and play Factorio or Rimworld. I don't
+      even code side projects even though i am a software engineer. But other people
+      seems to have activities such as hiking, traveling, dancing, socializing, gaming ... etc.
+    </p>
+
+    <p>
+      I think for one other people's activities are just as meaningless like mine.
+      They just have sole purpose of lifting enjoyment to the person, its a pleasure.
+      But watching youtube or gaming for me stopped being pleasurable, but i am still doing
+      them. Because there is nothing else to do.
+    </p>
+
+    <p>
+      In pursuit of productivity and expense reduction. I have refused to take up any distracting
+      activities that would take my time. Until today i have reduced them to only these activity.
+      These last 2 activity has been real hard to let go of. Why?
+    </p>
+
+    <p>
+      I think i may have been conditioned to consume Youtube all day. Even when its painful,
+      time consuming, no benefitial results. But i think deep down i am doing them because
+      its meaningful to me. My eyes, ears and brain like videos that are interesting. It has a
+      quota that needs to be consumed on a daily basis. 
+    </p>
+
+    <p>
+      So in order to have a solution to this problem i would need to first dismantle the problem.
+    </p>
+
+    <h4>Problem 1: Non-Benefitial</h4>
+
+    <p>
+      Even though it seems like I am gaining information. I am not, it just gets forgotten about
+      in 1 minute after it ends. So consumption is not benefitial to me but its benefitial to the
+      platform. It is a valuable product but consumption benefit is just going to other direction.
+    </p>
+
+    <p>
+      So I have to watch videos that are clear benefit to me by consuming it. Like direct solution
+      to my problems in everyday like. It still doesn't clear the non-benefitial problem but i think
+      it will atleast lower it.
+    </p>
+
+    <h4>Problem 2: Non-stop Consumption</h4>
+
+    <p>
+      Even if i want to stop, i can't. I physically can't, i don't know why. I tried lot of times
+      but i keep coming back to it.
+    </p>
+
+    <p>
+      I think the problem lies in type of activity i am doing. It is not survival activity, it is a 
+      leasure activity. Something what rich people do when they are bored. But i am not rich, ironically
+      acting like a rich person by watching videos leasurely.
+    </p>
+
+    <p>
+      So I have to come up with a way to snap my brain to survival mode. In survival mode, you would
+      literally see any activity if it has value to you just by looking in mere seconds. And you would act
+      on it. I have to wake my brain from laziness to survival high stress mode.
+    </p>
+
+    <h4>Problem 3: When no pleasure</h4>
+
+    <p>
+      If i am successful in removing these activities, i would be so miserable. I would need some activities
+      like this again just to feel some sense of pleasure. This is the hard problem i don't know how to solve
+      for now. Maybe just endure it, or planting some discipline to my brain? i don't know. I have to think about
+      it later.
+    </p>
+
+    <p>
+      - Acep
+    </p>
+
+    <small>2024, April - 04:18</small>
+  </article>
+</template>
