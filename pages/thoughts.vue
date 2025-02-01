81c139926debb9b23e631c34cda5ac52dbb2caa4
tsogtoodev <tsogtoo.dev@gmail.com>
2025-02-01 23:52:28 +0800

    new thought

--- pages/thoughts.vue
+++ pages/thoughts.vue
@@ -1,9 +1,16 @@
 <template>
   <UContainer class="py-2">
     <div class="flex flex-row">
-      <div class="flex flex-col w-64">
-        <span class="text-gray-400 text-sm">2024, April</span>
-        <UVerticalNavigation :links="links" />
+      <div class="flex flex-col w-64 gap-4">
+        <div class="flex flex-col gap-2">
+          <span class="text-gray-400 text-sm">2025, February</span>
+          <UVerticalNavigation :links="links2025February" />
+        </div>
+
+        <div class="flex flex-col gap-2">
+          <span class="text-gray-400 text-sm">2024, April</span>
+          <UVerticalNavigation :links="links2024April" />
+        </div>
       </div>
 
       <NuxtPage />
@@ -12,7 +19,10 @@
 </template>
 
 <script setup lang="ts">
-const links = [
+const links2025February = [
+  {label: "- Absolute Despair", to: "/thoughts/absolute-despair"},
+];
+const links2024April = [
   {label: "- Meaningful Activity", to: "/thoughts/meaningful-activity"},
   {label: "- Rage & Violence", to: "/thoughts/rage-violence"},
   {label: "- Admitting Defeat", to: "/thoughts/admitting-defeat"},
---
+++ pages/thoughts/absolute-despair.vue
@@ -0,0 +1,21 @@
+<template>
+  <article class="prose prose-sm mx-auto">
+    <h2>Absolute Despair</h2>
+    <small>2025, February 1st, 19:25</small>
+
+    <p>
+      Boiling point has been reached, all my justifications has been unfruitful.
+      What happened is that all my promises hopes have been unsuccessful, I have been
+      running on high hope that everything I did up to this point would have succeeded
+      and all this time expenditure, sacrifice and suffering would be justified.
+    </p>
+
+    <p>
+      But I was wrong, dead wrong!
+    </p>
+
+    <p>- xNiccolo</p>
+
+    <small>2025, February 1st, 19:25</small>
+  </article>
+</template>
